4202bf6f48c041f388ae6a23bb105f67c7948db1
garrett <yien.garrett94@gmail.com>
2021-07-06 20:55:43 -0500

    defined all objects to have event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+const navButtons = document.querySelectorAll("nav > a")
+console.log(navButtons);
+const homeButton = navButtons[0];
+const aboutButton = navButtons[1];
+const blogButton = navButtons[2];
+const contactButton = navButtons[3];
+
+const imgs = document.querySelectorAll('img');
+console.log(imgs)
+const busImg = imgs[0];
+const mapImg = imgs[1];
+const cityImg = imgs[2];
+const beachImg = imgs[3];
+
+const destinationsButtons = document.querySelectorAll("div.btn");
+const funButton = destinationsButtons[0];
+const mountainButton = destinationsButtons[1];
+const islandButton = destinationsButtons[2];
+console.log(destinationsButtons);
+
